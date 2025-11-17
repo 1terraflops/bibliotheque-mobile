0380a5e8d0de290920494b82627aea2820f033f8
vvovk <vvovk@stud.khpi.ucode-connect.study>
2025-11-17 22:53:08 +0200

    added new navigation tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,26 @@
 import { Tabs } from "expo-router";
+import { FC } from "react";
 import { ChartColumnStacked, House, Newspaper } from "lucide-react-native";
-import { Text, View } from "react-native";
 
-const _Layout = () => {
+const _Layout: FC = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "teal",
-        tabBarInactiveTintColor: "black",
-        tabBarLabelStyle: { fontSize: 12, marginTop: 2 },
+        headerShown: false,
+        tabBarActiveTintColor: "#D97126",
         tabBarStyle: {
-          height: 75,
+          backgroundColor: "#282b28",
           borderRadius: 100,
-          marginBottom: 12,
-          marginHorizontal: 12,
-          backgroundColor: "rgba(255, 224, 178, 0.55)",
-          boxShadow: "0px 0px 4px lightgray",
+          height: 70,
+          marginBottom: 24,
+          marginHorizontal: 60,
         },
-        tabBarItemStyle: {
-          marginTop: 10,
+        tabBarLabelStyle: {
+          fontWeight: 600,
+          marginTop: 2,
+        },
+        tabBarIconStyle: {
+          marginTop: 6,
         },
       }}
     >
@@ -26,108 +28,21 @@ const _Layout = () => {
         name="index"
         options={{
           title: "Home",
-          headerShown: false,
-          tabBarLabel: ({ focused }) => (
-            <Text
-              style={{ fontSize: 12, color: focused ? "#db563b" : "#363535" }}
-            >
-              Home
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
-              {focused && (
-                <View
-                  style={{
-                    position: "absolute",
-                    top: -15,
-                    width: 121,
-                    height: 75,
-                    borderRadius: 100,
-                    backgroundColor: "rgb(250, 215, 162)",
-                  }}
-                />
-              )}
-              <House
-                size={28}
-                stroke={focused ? "black" : "#363535"}
-                fill="#db563b"
-                fillOpacity={focused ? 0.4 : 0}
-              />
-            </View>
-          ),
+          tabBarIcon: ({ color }) => <House color={color} size={26} />,
         }}
       />
-
       <Tabs.Screen
         name="activity"
         options={{
           title: "Activity",
-          headerShown: false,
-          tabBarLabel: ({ focused }) => (
-            <Text style={{ fontSize: 12, color: focused ? "teal" : "#363535" }}>
-              Activity
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
-              {focused && (
-                <View
-                  style={{
-                    position: "absolute",
-                    top: -15,
-                    width: 121,
-                    height: 75,
-                    borderRadius: 100,
-                    backgroundColor: "rgb(250, 215, 162)",
-                  }}
-                />
-              )}
-              <Newspaper
-                size={28}
-                stroke={focused ? "black" : "#363535"}
-                fill="teal"
-                fillOpacity={focused ? 0.45 : 0}
-              />
-            </View>
-          ),
+          tabBarIcon: ({ color }) => <Newspaper color={color} />,
         }}
       />
-
       <Tabs.Screen
         name="stats"
         options={{
           title: "Stats",
-          headerShown: false,
-          tabBarLabel: ({ focused }) => (
-            <Text
-              style={{ fontSize: 12, color: focused ? "green" : "#363535" }}
-            >
-              Stats
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) => (
-            <View style={{ alignItems: "center", justifyContent: "center" }}>
-              {focused && (
-                <View
-                  style={{
-                    position: "absolute",
-                    top: -15,
-                    width: 121,
-                    height: 75,
-                    borderRadius: 100,
-                    backgroundColor: "rgb(250, 215, 162)",
-                  }}
-                />
-              )}
-              <ChartColumnStacked
-                size={28}
-                stroke={focused ? "black" : "#363535"}
-                fill="green"
-                fillOpacity={focused ? 0.25 : 0}
-              />
-            </View>
-          ),
+          tabBarIcon: ({ color }) => <ChartColumnStacked color={color} />,
         }}
       />
     </Tabs>
