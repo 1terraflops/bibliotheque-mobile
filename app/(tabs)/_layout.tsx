b819e220dfa500ad176ed970bf94d37ff53521ca
volodymyr <vvovk225@gmail.com>
2025-11-18 23:04:22 +0200

    added an avatar button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,19 +7,23 @@ const _Layout: FC = () => {
   return (
     <Tabs>
       <TabSlot />
-      <TabList className="bg-dark h-[68px] mb-6 mx-20 px-8 rounded-full items-center">
-        <TabTrigger asChild name="index" href="/(tabs)">
-          <TabButton icon={Book}>Books</TabButton>
-        </TabTrigger>
-        <TabTrigger asChild name="activity" href="/(tabs)/activity">
-          <TabButton icon={Newspaper}>Activity</TabButton>
-        </TabTrigger>
-        <TabTrigger asChild name="stats" href="/(tabs)/stats">
-          <TabButton icon={ChartColumnStacked}>Stats</TabButton>
-        </TabTrigger>
-      </TabList>
+      <>
+        <TabList className="absolute gap-x-12 bottom-6 left-[10%] bg-dark h-[68px] px-8 rounded-full flex-row items-center justify-between gap-6">
+          <TabTrigger asChild name="index" href="/(tabs)">
+            <TabButton icon={Book}>Books</TabButton>
+          </TabTrigger>
 
-      {/* {<TabList className="h-[64px] w-[64px] bg-dark rounded-full"></TabList>} */}
+          <TabTrigger asChild name="activity" href="/(tabs)/activity">
+            <TabButton icon={Newspaper}>Activity</TabButton>
+          </TabTrigger>
+
+          <TabTrigger asChild name="stats" href="/(tabs)/stats">
+            <TabButton icon={ChartColumnStacked}>Stats</TabButton>
+          </TabTrigger>
+        </TabList>
+
+        <TabList className="absolute bottom-6 right-[10%] h-[64px] w-[64px] bg-dark rounded-full items-center justify-center"></TabList>
+      </>
     </Tabs>
   );
 };
