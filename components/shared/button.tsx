a3bf47ecd3cc8ab1fef1a109a4b5d76b27bab49f
volodymyr <vvovk225@gmail.com>
2026-01-19 16:48:13 +0200

    added button component

---
+++ components/shared/button.tsx
@@ -0,0 +1,114 @@
+import { colors } from "@/constants/colors";
+import { cn } from "@/utils/cn";
+import { LucideIcon } from "lucide-react-native";
+import { FC, ReactNode } from "react";
+import {
+  ActivityIndicator,
+  Pressable,
+  PressableProps,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+
+type variant = "primary" | "secondary" | "icon" | "text";
+
+const ButtonStyles = {
+  light: {
+    primary: "w-full py-4 rounded-[10px] bg-dark-1",
+    secondary: "w-full py-4 rounded-[10px] border border-dark-1",
+    icon: "",
+    text: "",
+  },
+  dark: {
+    primary: "w-full py-4 rounded-[10px] bg-light-1",
+    secondary: "w-full py-4 rounded-[10px] border border-light-1",
+    icon: "",
+    text: "",
+  },
+};
+
+const TextButtonStyles = {
+  light: {
+    primary: "font-inter-500 text-xl text-light-1",
+    secondary: "font-inter-500 text-xl text-dark-1",
+    icon: "",
+    text: "font-inter-500 text-lg text-dark-1",
+  },
+  dark: {
+    primary: "font-inter-500 text-xl text-dark-1",
+    secondary: "font-inter-500 text-xl text-light-1",
+    icon: "",
+    text: "font-inter-500 text-lg text-light-1",
+  },
+};
+
+const IconColors = {
+  light: {
+    primary: colors["light-1"],
+    secondary: colors["dark-1"],
+    icon: colors["dark-1"],
+    text: "",
+  },
+  dark: {
+    primary: colors["dark-1"],
+    secondary: colors["light-1"],
+    icon: colors["light-1"],
+    text: "",
+  },
+};
+
+interface ButtonProps extends PressableProps {
+  children?: ReactNode;
+  variant?: variant;
+  title?: string;
+  iconLeft?: LucideIcon;
+  iconRight?: LucideIcon;
+  loading?: boolean;
+}
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  variant = "primary",
+  title,
+  iconLeft: IconLeft,
+  iconRight: IconRight,
+  loading = false,
+  className,
+  ...props
+}) => {
+  const theme = useColorScheme() ?? "light";
+
+  return (
+    <View className={className}>
+      <Pressable
+        {...props}
+        disabled={loading}
+        className={cn(
+          "flex-row justify-center gap-x-2",
+          ButtonStyles[theme][variant],
+          loading && "opacity-80",
+        )}
+      >
+        {!loading && variant !== "text" && IconLeft && (
+          <IconLeft color={IconColors[theme][variant]} />
+        )}
+        {!loading && variant !== "icon" && (
+          <Text className={TextButtonStyles[theme][variant]}>
+            {children ?? title}
+          </Text>
+        )}
+        {!loading && variant !== "text" && IconRight && (
+          <IconRight color={IconColors[theme][variant]} />
+        )}
+        {loading && (
+          <ActivityIndicator
+            animating={loading}
+            color={IconColors[theme][variant]}
+            size={24}
+          />
+        )}
+      </Pressable>
+    </View>
+  );
+};
