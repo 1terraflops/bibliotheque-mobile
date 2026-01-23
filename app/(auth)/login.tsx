9499c2412ffc6afa9f66f29436cd567d47a96ad9
volodymyr <vvovk225@gmail.com>
2026-01-24 01:41:54 +0200

    updated login form and page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,18 +1,13 @@
-import { LoginForm } from "@/components/auth";
-import { cn } from "@/utils/cn";
-import { useColorScheme, View } from "react-native";
+import { LoginForm } from "@/components/auth/login";
+import { PageLayout } from "@/components/shared";
+import { View } from "react-native";
 
 export default function Login() {
-  const isLightTheme = useColorScheme() === "light";
-
   return (
-    <View
-      className={cn(
-        "flex-1 justify-center items-center bg-dark-1",
-        isLightTheme && "bg-light-1",
-      )}
-    >
-      <LoginForm />
-    </View>
+    <PageLayout backButton>
+      <View className="flex-1 justify-center items-center">
+        <LoginForm />
+      </View>
+    </PageLayout>
   );
 }
--- components/auth/login/login-form.tsx
+++ components/auth/login/login-form.tsx
@@ -1,11 +1,11 @@
 import { supabase } from "@/api/supabase";
-import { Button, Input } from "@/components/shared";
-import { LoginRequestSchema } from "@/types/auth";
+import { Button, Input, Typography } from "@/components/shared";
 import { useForm } from "@tanstack/react-form";
 import { useRouter } from "expo-router";
 import { Lock, Mail } from "lucide-react-native";
 import { useState } from "react";
 import { Text, View } from "react-native";
+import { ILoginForm, ILoginFormSchema } from "./model/model";
 
 export const LoginForm = () => {
   const [loading, setLoading] = useState(false);
@@ -16,9 +16,9 @@ export const LoginForm = () => {
     defaultValues: {
       email: "",
       password: "",
-    },
+    } satisfies ILoginForm,
     validators: {
-      onChange: LoginRequestSchema,
+      onChange: ILoginFormSchema,
     },
     onSubmit: async ({ value }) => {
       const { email, password } = value;
@@ -40,38 +40,18 @@ export const LoginForm = () => {
   });
 
   return (
-    <View>
-      <form.Field name="email">
-        {(field) => (
-          <Input
-            placeholderAsLabel
-            icon={Mail}
-            placeholder="Email"
-            value={field.state.value ?? ""}
-            onBlur={() => {
-              field.handleBlur();
-              setServerError(null);
-            }}
-            onChangeText={(text) => field.setValue(text)}
-            error={field.state.meta.errors[0]?.message}
-            showError={
-              !!serverError ||
-              (field.state.meta.isDirty &&
-                field.state.meta.isBlurred &&
-                field.state.meta.errors.length > 0)
-            }
-            isBlurred={field.state.meta.isBlurred}
-          />
-        )}
-      </form.Field>
-      <form.Field name="password">
-        {(field) => (
-          <>
+    <View className="flex-1 w-full items-center">
+      <View className="w-full items-center mt-auto">
+        <Typography className="text-center font-nunito-sans-800 text-4xl mb-6">
+          Welcome Back!
+        </Typography>
+
+        <form.Field name="email">
+          {(field) => (
             <Input
               placeholderAsLabel
-              secure
-              placeholder="Password"
-              icon={Lock}
+              icon={Mail}
+              placeholder="Email"
               value={field.state.value ?? ""}
               onBlur={() => {
                 field.handleBlur();
@@ -87,18 +67,51 @@ export const LoginForm = () => {
               }
               isBlurred={field.state.meta.isBlurred}
             />
-          </>
-        )}
-      </form.Field>
-      <Text className="font-nunito-sans text-attention-5 text-sm">
-        {serverError}
-      </Text>
-      <Button
-        onPress={form.handleSubmit}
-        title="Login"
-        variant="primary"
-        loading={loading}
-      />
+          )}
+        </form.Field>
+
+        <form.Field name="password">
+          {(field) => (
+            <>
+              <Input
+                placeholderAsLabel
+                secure
+                placeholder="Password"
+                icon={Lock}
+                value={field.state.value ?? ""}
+                onBlur={() => {
+                  field.handleBlur();
+                  setServerError(null);
+                }}
+                onChangeText={(text) => field.setValue(text)}
+                error={field.state.meta.errors[0]?.message}
+                showError={
+                  !!serverError ||
+                  (field.state.meta.isDirty &&
+                    field.state.meta.isBlurred &&
+                    field.state.meta.errors.length > 0)
+                }
+                isBlurred={field.state.meta.isBlurred}
+              />
+            </>
+          )}
+        </form.Field>
+      </View>
+
+      <Button variant="text" title="I forgot my Password" />
+
+      <View className="mt-auto gap-y-4">
+        <Text className="font-nunito-sans text-attention-5 text-center">
+          {serverError}
+        </Text>
+
+        <Button
+          onPress={form.handleSubmit}
+          title="Login"
+          variant="primary"
+          loading={loading}
+        />
+      </View>
     </View>
   );
 };
