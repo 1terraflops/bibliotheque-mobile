280311ccd1c6de91e1e157bfbbe16d8fbaea3252
volodymyr <vvovk225@gmail.com>
2026-02-14 16:08:24 +0200

    added skeleton loader to typography component

--- components/shared/typography.tsx
+++ components/shared/typography.tsx
@@ -1,25 +1,46 @@
 import { cn } from "@/utils/cn";
+import { Skeleton } from "moti/skeleton";
 import { FC, ReactNode } from "react";
 import { Text, TextProps, useColorScheme } from "react-native";
 
 interface TypographyProps extends TextProps {
   children?: ReactNode;
   className?: string;
+  hideSkeleton?: boolean;
+  skeletonWidth?: number;
 }
 
 export const Typography: FC<TypographyProps> = ({
   children,
   className,
+  hideSkeleton = false,
+  skeletonWidth = 96,
   ...props
 }) => {
-  const isLightTheme = useColorScheme() === "light";
+  const colorScheme = useColorScheme();
+  const colorMode = colorScheme === "dark" ? "dark" : "light";
+
+  if (hideSkeleton && children == null) {
+    return null;
+  }
 
   return (
-    <Text
-      {...props}
-      className={cn(isLightTheme ? "text-dark-1" : "text-light-1", className)}
+    <Skeleton
+      colorMode={colorMode}
+      width={children != null ? undefined : skeletonWidth}
+      height={children != null ? undefined : 24}
     >
-      {children}
-    </Text>
+      {children != null ? (
+        <Text
+          {...props}
+          className={cn(
+            colorMode === "light" ? "text-dark-1" : "text-light-1",
+            className,
+          )}
+        >
+          {children ?? null}
+        </Text>
+      ) : null}
+    </Skeleton>
   );
 };
