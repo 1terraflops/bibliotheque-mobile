8a0c06ce07c362a9ca34dc1ccf18647b9e08a8c6
volodymyr <vvovk225@gmail.com>
2026-03-12 19:22:43 +0200

    feat: added real books to dashboard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
-import { Button, ScreenLayout } from "@/components/shared";
+import { getBooksByStatusesQueryOptions } from "@/api/books/get-books-by-statuses.query";
+import { Button, ScreenLayout, Typography } from "@/components/shared";
+import { BookCard } from "@/components/shared/widgets";
+import { formatStatus } from "@/utils/formatStatus";
+import { useQuery } from "@tanstack/react-query";
 import { router } from "expo-router";
-import { Plus } from "lucide-react-native";
+import { ChevronRight, Plus } from "lucide-react-native";
+import { ScrollView, View } from "react-native";
 
 export default function Index() {
+  const { data: booksByStatuses } = useQuery(getBooksByStatusesQueryOptions());
+
+  const filteredBooks = booksByStatuses?.filter((book) => book.books.length);
+
   return (
     <ScreenLayout
+      scrollable
       title="Library"
       rightButton={
         <Button
@@ -14,6 +24,28 @@ export default function Index() {
           onPress={() => router.push("/add-book")}
         />
       }
-    ></ScreenLayout>
+    >
+      {filteredBooks?.map((status) => (
+        <View key={status.status} className="gap-4 mt-6">
+          <View className="flex-row gap-2 items-center">
+            <Typography className="text-2xl font-inter-700">
+              {formatStatus(status.status)}
+            </Typography>
+
+            <Button variant="icon" iconLeft={ChevronRight} iconSize={20} />
+          </View>
+
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerClassName="gap-6 px-1"
+          >
+            {status.books.map((item) => (
+              <BookCard key={item.book.isbn} book={item.book} />
+            ))}
+          </ScrollView>
+        </View>
+      ))}
+    </ScreenLayout>
   );
 }
