d0c495839f3281b24908600bca0be0ee275fe41d
volodymyr <vvovk225@gmail.com>
2026-03-15 01:00:28 +0200

    fix: added export for progress bar component

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,6 +1,7 @@
 export * from "./avatar";
 export * from "./button";
 export * from "./input";
+export * from "./progress-bar";
 export * from "./screen-layout";
 export * from "./spinner";
 export * from "./typography";
