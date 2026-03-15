f2ba10f1e982c1c5e95f495f70d516e6bf7cc01b
volodymyr <vvovk225@gmail.com>
2026-03-15 18:53:33 +0200

    fix: updated delete mutation

--- api/books/delete-book.mutation.ts
+++ api/books/delete-book.mutation.ts
@@ -1,27 +1,48 @@
-import { UserBookSchema } from "@/types/books";
+import { UserBook, UserBookSchema } from "@/types/books";
 import { parseResponse } from "@/utils/parseResponse";
 import { mutationOptions } from "@tanstack/react-query";
 import { api } from "../axios";
 import { queryClient } from "../queryClient";
 import { getBooksByStatusesQueryOptions } from "./get-books-by-statuses.query";
+import { getBooksInfiniteQueryOptions } from "./get-books.query";
 
 export const deleteBookMutationOptions = () =>
   mutationOptions({
-    mutationFn: async (isbn: string) =>
-      await api.delete(`books/${isbn}`).then(parseResponse(UserBookSchema)),
-    onMutate: (isbn: string) => {
+    mutationFn: async (book: UserBook) =>
+      await api
+        .delete(`books/${book.book.isbn}`)
+        .then(parseResponse(UserBookSchema)),
+    onMutate: (book: UserBook) => {
       queryClient.setQueryData(
         getBooksByStatusesQueryOptions().queryKey,
         (old) =>
           old?.map((status) => ({
             ...status,
-            books: status.books.filter((b) => b.book.isbn !== isbn),
+            books: status.books.filter((b) => b.book.isbn !== book.book.isbn),
           })),
       );
+
+      queryClient.setQueriesData(
+        { queryKey: getBooksInfiniteQueryOptions({}).queryKey },
+        (old: any) => {
+          if (!old) return old;
+          return {
+            ...old,
+            pages: old.pages.map((page: UserBook[]) =>
+              page.filter((b) => b.book.isbn !== book.book.isbn),
+            ),
+          };
+        },
+      );
     },
-    onError: () => {
+    onError: (_, ctx) => {
       queryClient.invalidateQueries({
         queryKey: getBooksByStatusesQueryOptions().queryKey,
+      });
+      queryClient.invalidateQueries({
+        queryKey: getBooksInfiniteQueryOptions({
+          status: ctx.status,
+        }).queryKey,
       });
     },
   });
