fb899978160607b53fc7906415655327a70c37db
volodymyr <vvovk225@gmail.com>
2026-03-21 19:13:07 +0200

    feat: added flashlight for camera view and fixed double modals

--- app/(modals)/scan-isbn.tsx
+++ app/(modals)/scan-isbn.tsx
@@ -1,12 +1,13 @@
-import { Spinner } from "@/components/shared";
+import { Button, Spinner } from "@/components/shared";
 import { CameraView, useCameraPermissions } from "expo-camera";
 import { router } from "expo-router";
-import { X } from "lucide-react-native";
-import { useEffect, useRef } from "react";
-import { Pressable, View } from "react-native";
+import { Flashlight, FlashlightOff, X } from "lucide-react-native";
+import { useEffect, useRef, useState } from "react";
+import { View } from "react-native";
 
 export default function ScanISBN() {
   const [permission, requestPermission] = useCameraPermissions();
+  const [torch, setTorch] = useState(false);
   const scanned = useRef(false);
 
   useEffect(() => {
@@ -25,27 +26,42 @@ export default function ScanISBN() {
     router.back();
   }
 
+  const toggleTorch = () => {
+    setTorch((prev) => !prev);
+  };
+
   return (
     <View className="relative flex-1">
       <CameraView
         zoom={0.1}
         style={{ flex: 1 }}
+        enableTorch={torch}
         barcodeScannerSettings={{ barcodeTypes: ["ean8", "ean13"] }}
         onBarcodeScanned={({ data }) => {
           if (scanned.current) return;
           scanned.current = true;
-          router.replace({
+
+          router.dismiss(2);
+          router.push({
             pathname: "/(modals)/add-book",
             params: { isbn: data },
           });
         }}
       />
-      <Pressable
-        onPress={() => router.back()}
-        className="absolute top-14 right-6 bg-black/50 rounded-full p-2"
-      >
-        <X color="white" size={24} />
-      </Pressable>
+      <View className="flex flex-row gap-3 item-center absolute top-16 right-6">
+        <Button
+          variant="icon"
+          iconLeft={torch ? Flashlight : FlashlightOff}
+          className="bg-black/50 rounded-full p-3"
+          onPress={toggleTorch}
+        />
+        <Button
+          variant="icon"
+          iconLeft={X}
+          className="bg-black/50 rounded-full p-3"
+          onPress={() => router.back()}
+        />
+      </View>
     </View>
   );
 }
