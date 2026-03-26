d3d930692787d1b2cd00fa01430f90881295d81e
volodymyr <vvovk225@gmail.com>
2026-03-26 22:22:04 +0200

    feat: added cancel session tab

--- components/shared/modals/end-session-modal.tsx
+++ components/shared/modals/end-session-modal.tsx
@@ -1,3 +1,4 @@
+import { cancelSessionMutationOptions } from "@/api/reading-sessions/cancel-session.mutation";
 import { endSessionMutationOptions } from "@/api/reading-sessions/end-session.mutation";
 import { getActiveSessionQueryOptions } from "@/api/reading-sessions/get-active-session.query";
 import {
@@ -9,7 +10,7 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 import { useForm } from "@tanstack/react-form";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import moment from "moment";
-import { FC } from "react";
+import { FC, useState } from "react";
 import {
   Keyboard,
   TouchableWithoutFeedback,
@@ -19,17 +20,28 @@ import {
 import { ModalProps } from "react-native-modalfy";
 import { Button } from "../button";
 import { Input } from "../input";
+import { Tabs, TabsOptions } from "../tabs";
 import { Typography } from "../typography";
+
+enum END_SESSION_TABS {
+  END_SESSION = "END_SESSION",
+  CANCEL_SESSION = "CANCEL_SESSION",
+}
 
 type EndSessionModalProps = ModalProps<"EndSession">;
 
 export const EndSessionModal: FC<EndSessionModalProps> = ({ modal }) => {
   const { params, closeModal } = modal;
+  const [activeTab, setActiveTab] = useState(END_SESSION_TABS.END_SESSION);
+
   const isLightTheme = useColorScheme() === "light";
+
   const { data: session } = useQuery(getActiveSessionQueryOptions());
+
   const { mutate: endSession, isPending } = useMutation(
     endSessionMutationOptions(),
   );
+  const { mutate: cancelSession } = useMutation(cancelSessionMutationOptions());
 
   const form = useForm({
     defaultValues: {
@@ -57,6 +69,11 @@ export const EndSessionModal: FC<EndSessionModalProps> = ({ modal }) => {
     },
   });
 
+  const tabs: TabsOptions[] = [
+    { label: "End Session", value: END_SESSION_TABS.END_SESSION },
+    { label: "Cancel Session", value: END_SESSION_TABS.CANCEL_SESSION },
+  ];
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View
@@ -67,94 +84,118 @@ export const EndSessionModal: FC<EndSessionModalProps> = ({ modal }) => {
             : "bg-dark-1 border-dark-3",
         )}
       >
-        <Typography className="text-2xl font-inter-600 tracking-tight mb-3">
-          End Session
-        </Typography>
+        <Tabs
+          tabs={tabs}
+          activeTabId={activeTab}
+          onTabChange={(tab) => setActiveTab(tab as END_SESSION_TABS)}
+          size="medium"
+        />
 
-        <View className="flex flex-row gap-2">
-          <View className="flex-1">
-            <form.Field name="startPage">
+        {activeTab === END_SESSION_TABS.END_SESSION && (
+          <View className="mt-4">
+            <View className="flex flex-row gap-2">
+              <View className="flex-1">
+                <form.Field name="startPage">
+                  {(field) => (
+                    <Input
+                      label="Start Page"
+                      returnKeyType="done"
+                      keyboardType="number-pad"
+                      value={
+                        field.state.value === 0 ? "" : String(field.state.value)
+                      }
+                      onBlur={field.handleBlur}
+                      onChangeText={(text) =>
+                        field.setValue(text === "" ? 0 : Number(text))
+                      }
+                      error={field.state.meta.errors[0]?.message}
+                      showError={
+                        field.state.meta.isDirty &&
+                        field.state.meta.isBlurred &&
+                        field.state.meta.errors.length > 0
+                      }
+                      isBlurred={field.state.meta.isBlurred}
+                    />
+                  )}
+                </form.Field>
+              </View>
+
+              <View className="flex-1">
+                <form.Field name="endPage">
+                  {(field) => (
+                    <Input
+                      autoFocus
+                      label="End Page"
+                      returnKeyType="done"
+                      keyboardType="number-pad"
+                      value={
+                        field.state.value === 0 ? "" : String(field.state.value)
+                      }
+                      onBlur={field.handleBlur}
+                      onChangeText={(text) =>
+                        field.setValue(text === "" ? 0 : Number(text))
+                      }
+                      error={field.state.meta.errors[0]?.message}
+                      showError={
+                        field.state.meta.isDirty &&
+                        field.state.meta.isBlurred &&
+                        field.state.meta.errors.length > 0
+                      }
+                      isBlurred={field.state.meta.isBlurred}
+                    />
+                  )}
+                </form.Field>
+              </View>
+            </View>
+
+            <form.Field name="finishedAt">
               {(field) => (
-                <Input
-                  label="Start Page"
-                  returnKeyType="done"
-                  keyboardType="number-pad"
-                  value={
-                    field.state.value === 0 ? "" : String(field.state.value)
-                  }
-                  onBlur={field.handleBlur}
-                  onChangeText={(text) =>
-                    field.setValue(text === "" ? 0 : Number(text))
-                  }
-                  error={field.state.meta.errors[0]?.message}
-                  showError={
-                    field.state.meta.isDirty &&
-                    field.state.meta.isBlurred &&
-                    field.state.meta.errors.length > 0
-                  }
-                  isBlurred={field.state.meta.isBlurred}
-                />
+                <View className="items-center">
+                  <Typography className="font-roboto-mono text-lg">
+                    Ended At
+                  </Typography>
+                  <DateTimePicker
+                    minimumDate={new Date(session?.startedAt ?? "")}
+                    maximumDate={new Date(moment().toISOString())}
+                    value={new Date(field.state.value)}
+                    mode="time"
+                    display="spinner"
+                    is24Hour={true}
+                    onChange={(_: unknown, date?: Date) => {
+                      if (date) field.setValue(date.toISOString());
+                    }}
+                  />
+                </View>
               )}
             </form.Field>
-          </View>
 
-          <View className="flex-1">
-            <form.Field name="endPage">
-              {(field) => (
-                <Input
-                  autoFocus
-                  label="End Page"
-                  returnKeyType="done"
-                  keyboardType="number-pad"
-                  value={
-                    field.state.value === 0 ? "" : String(field.state.value)
-                  }
-                  onBlur={field.handleBlur}
-                  onChangeText={(text) =>
-                    field.setValue(text === "" ? 0 : Number(text))
-                  }
-                  error={field.state.meta.errors[0]?.message}
-                  showError={
-                    field.state.meta.isDirty &&
-                    field.state.meta.isBlurred &&
-                    field.state.meta.errors.length > 0
-                  }
-                  isBlurred={field.state.meta.isBlurred}
-                />
-              )}
-            </form.Field>
-          </View>
-        </View>
-
-        <form.Field name="finishedAt">
-          {(field) => (
-            <View className="items-center">
-              <Typography className="font-roboto-mono text-lg">
-                Ended At
-              </Typography>
-              <DateTimePicker
-                minimumDate={new Date(session?.startedAt ?? "")}
-                maximumDate={new Date(moment().toISOString())}
-                value={new Date(field.state.value)}
-                mode="time"
-                display="spinner"
-                is24Hour={true}
-                onChange={(_: unknown, date?: Date) => {
-                  if (date) field.setValue(date.toISOString());
-                }}
+            <View className="flex-row gap-4 mt-6">
+              <Button
+                title="End Session"
+                className="flex-1"
+                loading={isPending}
+                onPress={() => form.handleSubmit()}
               />
             </View>
-          )}
-        </form.Field>
+          </View>
+        )}
 
-        <View className="flex-row gap-4 mt-6">
-          <Button
-            title="End Session"
-            className="flex-1"
-            loading={isPending}
-            onPress={() => form.handleSubmit()}
-          />
-        </View>
+        {activeTab === END_SESSION_TABS.CANCEL_SESSION && (
+          <View>
+            <Typography className="text-lg my-4">
+              Are you sure you want to cancel this session?
+            </Typography>
+
+            <Button
+              destructive
+              title="Yes, Cancel Session"
+              onPress={() => {
+                cancelSession();
+                closeModal();
+              }}
+            />
+          </View>
+        )}
       </View>
     </TouchableWithoutFeedback>
   );
